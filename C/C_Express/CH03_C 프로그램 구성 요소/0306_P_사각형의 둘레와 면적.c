630af183e12e5387afa77578ceb956c62a6c318f
naylle98 <khb9827y@naver.com>
2022-01-19 21:48:07 +0900

    .

---
+++ C/C_Express/CH03_C 프로그램 구성 요소/0306_P_사각형의 둘레와 면적.c
@@ -0,0 +1,37 @@
+﻿/*
+실습. 사각형의 둘레와 면적
+
+직사각형의 둘레와 면적을 구하는 프로그램을 작성하여 보자.
+직사각형의 가로와 세로를 각각 w와 h라고 하면 직사각형의 면적은 w*h가 되고 둘레는 2*(w+h)가 된다.
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	double w;
+	double h;
+	double area;
+	double perimeter;
+
+	// 도전 2
+	// double w, h, area, perimeter;
+
+	// 도전 3
+	// printf("가로 세로 길이 입력: ");
+	// scanf_s("%lf %lf", &w, &h);
+	
+	w = 10.0;
+	h = 5.0;
+	area = w * h;
+	perimeter = 2 * (w + h);
+
+	printf("사각형의 넓이: %lf\n", area);
+	printf("사각형의 둘레: %lf\n", perimeter);
+
+	// 도전 1
+	// printf("사각형의 넓이: %lf \n사각형의 둘레: %lf\n", area, perimeter);
+
+	return 0;
+}
---
+++ C/C_Express/CH03_Programming/Ex_0301.c
@@ -0,0 +1,30 @@
+﻿/*
+0301.
+
+사용자로부터 세 개의 실수를 입력받은 후, 합계와 평균값을 계산하여 화면에 출력하는 프로그램을 작성하라.
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	float x, y, z; // 세 개의 실수
+	float sum, avg; // 합계, 평균
+
+	printf("실수를 입력하시오: ");
+	scanf_s("%f", &x);
+
+	printf("실수를 입력하시오: ");
+	scanf_s("%f", &y);
+
+	printf("실수를 입력하시오: ");
+	scanf_s("%f", &z);
+
+	sum = x + y + z;
+	avg = sum / 3;
+
+	printf("합계는 %f이고 평균값은 %f입니다.\n", sum, avg);
+
+	return 0;
+}
