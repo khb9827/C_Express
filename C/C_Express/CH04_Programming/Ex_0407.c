ac1af6858c0bf8f85ac13a4999736b862a186d8f
naylle98 <khb9827y@naver.com>
2022-02-03 20:24:55 +0900

    .

---
+++ C/C_Express/CH04_Programming/Ex_0407.c
@@ -0,0 +1,28 @@
+﻿/*
+0407.
+
+물리학에서 운동에너지는 E=mv^2/2.0으로 계산된다.
+사용자로부터 질량(m)과 속도(v)를 받아서 운동에너지(E)를 계산하는 프로그램을 작성하여 보자.
+모든 변수는 dobule형을 사용하라.
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	double m; // 질량
+	double v; // 속도
+	double e; // 운동 에너지
+
+	printf("질량: ");
+	scanf_s("%lf", &m);
+
+	printf("속도: ");
+	scanf_s("%lf", &v);
+
+	e = m * v*v / 2.0;
+	printf("운동에너지: %lf \n",e);
+
+	return 0;
+}
