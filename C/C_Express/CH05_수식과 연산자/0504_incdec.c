f3dea4eb89cc556f012f7a1d635ef66417a40000
naylle98 <khb9827y@naver.com>
2022-02-07 22:36:52 +0900

    .

---
+++ C/C_Express/CH05_수식과 연산자/0504_incdec.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int x = 10, y = 10;
+
+	printf("x=%d\n", x);
+	printf("++x의 값=%d\n", ++x); // 먼저 증가되고 증가된 값이 수식에 사용
+	printf("x=%d\n", x);
+
+	printf("y=%d\n", y);
+	printf("y++의 값=%d\n", y++); // 현재 값을 먼저 수식에 사용하고 나중에 증가됨
+	printf("y=%d\n", y);
+
+	return 0;
+}
