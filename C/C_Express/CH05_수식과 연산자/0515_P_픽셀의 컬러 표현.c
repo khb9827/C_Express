9a5fe710a6caf1f6c62de0b26b74af363cbc5e82
naylle98 <khb9827y@naver.com>
2022-02-09 19:26:57 +0900

    .

---
+++ C/C_Express/CH05_수식과 연산자/0515_P_픽셀의 컬러 표현.c
@@ -0,0 +1,17 @@
+﻿/*
+실습. 픽셀의 컬러 표현
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	unsigned int color = 0x00380000; // 픽셀의 색상
+	unsigned int result;
+
+	printf("픽셀의 색상: %#08X\n", color);
+	result = color & 0x00ff0000; // 마스크 연산
+	result = result >> 16; // 비트 이동 연산
+	printf("추출된 빨강색: %#08X\n", result);
+
+	return 0;
+}
