84613ddf0db1b78c46a6f13af59cbe435e6b317d
naylle98 <khb9827y@naver.com>
2022-02-21 21:06:51 +0900

    .

---
+++ C/C_Express/CH06_조건문/0610_days_in_month.c
@@ -0,0 +1,34 @@
+﻿// 달의 일수를 계산하는 프로그램
+#include <stdio.h>
+
+int main(void)
+{
+	int month, days;
+
+	printf("달을 입력하시오: ");
+	scnaf_s("%d", &month);
+
+	switch (month)
+	{
+	case 2:
+		days = 28;
+		break;
+
+	// 의도적으로 break문 생략
+	case 4:
+	case 6:
+	case 9:
+	case 11:
+		days = 30;
+		break;
+	
+	// 1,3,5,7,8,10,12월 처리
+	default:
+		days = 31;
+		break;
+	}
+
+	printf("%d월의 일수는 %d입니다.\n", days);
+
+	return 0;
+}
---
+++ C/C_Express/CH06_조건문/0611_P_산술계산기2.c
@@ -0,0 +1,44 @@
+﻿/*
+실습. 산술계산기#2
+
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	char op;
+	int x, y, result;
+
+	printf("수식을 입력하시오\n");
+	printf("(예: 2 + 5)\n");
+	printf(">>");
+	scanf("%d %c %d", &x, &op, &y);
+
+	switch (op)
+	{
+	case '+':
+		result = x + y;
+		break;
+
+	case '-':
+		result = x - y;
+		break;
+
+	case '*':
+		result = x * y;
+		break;
+
+	case '/':
+		result = x / y;
+		break;
+
+	case '%':
+		result = x % y;
+		break;
+	}
+
+	printf("%d %c %d = %d\n", x, op, y, result);
+
+	return 0;
+}
