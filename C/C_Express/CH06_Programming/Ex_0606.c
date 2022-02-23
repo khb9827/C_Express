e74f31b54b8bf5fdcbdec5a8e6ff739c18fe2a81
naylle98 <khb9827y@naver.com>
2022-02-23 20:58:54 +0900

    .

---
+++ C/C_Express/CH06_Programming/Ex_0606.c
@@ -0,0 +1,69 @@
+﻿/*
+0606.
+
+사용자로부터 몇 월인지를 정수로 입력받아서 Jan와 같은 영어단어로 출력하는 프로그램을 작성하라.
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int mon; // 월
+
+	printf("월번호를 입력하시오(1-12): ");
+	scanf_s("%d", &mon);
+
+	switch (mon)
+	{
+	case 1:
+		printf("Jan\n");
+		break;
+
+	case 2:
+		printf("Feb\n");
+		break;
+
+	case 3:
+		printf("Mar\n");
+		break;
+
+	case 4:
+		printf("Apr\n");
+		break;
+
+	case 5:
+		printf("May\n");
+		break;
+
+	case 6:
+		printf("Jun\n");
+		break;
+
+	case 7:
+		printf("Jul\n");
+		break;
+
+	case 8:
+		printf("Aug\n");
+		break;
+
+	case 9:
+		printf("Sep\n");
+		break;
+
+	case 10:
+		printf("Oct\n");
+		break;
+
+	case 11:
+		printf("Nov\n");
+		break;
+
+	case 12:
+		printf("Dec\n");
+		break;
+	}
+
+	return 0;
+}
---
+++ C/C_Express/CH06_Programming/Ex_0607.c
@@ -0,0 +1,27 @@
+﻿/*
+0607.
+
+사용자로부터 키를 입력받아서 표준 체중을 계산한 후에 사용자의 체중과 비교하여 저체중인지, 표준인지, 과체중인지를 판단하는 프로그램을 작성하라.
+표준 체중 계산식은 다음을 사용하라.
+
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	int cm, kg; // 키, 체중
+	int standard_weight; // 표준 체중
+	printf("체중과 키를 입력하시오(키, 체중): ");
+	scanf_s("%d %d", &cm, &kg);
+
+	standard_weight = (cm - 100)*0.9;
+
+	if (kg > standard_weight)
+		printf("과체중입니다.\n");
+	else if (kg < standard_weight)
+		printf("저체중입니다.\n");
+	else
+		printf("표준입니다.\n");
+
+	return 0;
+}
