06ce1a4ea213b7fa9ced15c10f64fce043d74b3c
naylle98 <khb9827y@naver.com>
2022-03-04 23:08:11 +0900

    .

---
+++ C/C_Express/CH07_반복문/0714_draw_box.c
@@ -0,0 +1,17 @@
+﻿// 반복을 이용한 네모 그리기
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	printf("**********\n");
+
+	// i가 0부터 5까지 5번 반복. 5가 되면 반복이 중지된다.
+	for (i = 0; i < 5; i++)
+		printf("*        *\n");
+
+	printf("**********\n");
+
+	return 0;
+}
---
+++ C/C_Express/CH07_반복문/0717_nested_loop2.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int x, y;
+
+	// 외부 반복문(5번 반복)
+	for (y = 1; y <= 5; y++)
+	{
+		// 내부 반복문(y번 반복)
+		for (x = 0; x < y; x++)
+			printf("*");
+
+		printf("\n");
+	}
+
+	return 0;
+}
