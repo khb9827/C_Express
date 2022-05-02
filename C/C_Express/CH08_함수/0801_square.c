43e2681af5233b90e5c41cda83291356f82560dc
naylle98 <khb9827y@naver.com>
2022-05-02 11:07:35 +0900

    .

---
+++ C/C_Express/CH08_함수/0801_square.c
@@ -0,0 +1,20 @@
+﻿// 정수의 제곱을 계산하는 함수 예제
+#include <stdio.h>
+
+int square(int n); // 함수 원형 선언
+
+int main(void)
+{
+	int result;
+
+	result = square(5); // 함수 호출, 함수 반환값이 result에 저장된다.
+	printf("%d \n", result);
+
+	return 0;
+}
+
+// 함수 정의
+int square(int n)
+{
+	return n * n;
+}
---
+++ C/C_Express/CH08_함수/0802_get_max.c
@@ -0,0 +1,22 @@
+﻿// 두수 중에서 큰 수를 찾는 함수 예제
+#include <stdio.h>
+int get_max(int x, int y);
+
+int main(void)
+{
+	int a, b;
+
+	printf("두개의 정수를 입력하시오: ");
+	scanf_s("%d %d", &a, &b);
+	printf("두 수 중에서 큰 수는 %d입니다.\n", get_max(a, b)); // 함수의 반환값 출력
+
+	return 0;
+}
+
+int get_max(int x, int y)
+{
+	if (x > y)
+		return (x);
+	else
+		return (y);
+}
