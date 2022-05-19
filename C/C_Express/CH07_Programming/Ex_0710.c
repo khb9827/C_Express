35d0384dcfe717ee367c44442903e29fc102a3e9
naylle98 <khb9827y@naver.com>
2022-05-19 15:46:13 +0900

    .

---
+++ C/C_Express/CH07_Programming/Ex_0710.c
@@ -0,0 +1,31 @@
+﻿/*
+0710.
+
+실수의 거듭 제곱값을 계산하는 프로그램을 작성하여 보자.
+사용자로부터 하나의 실수 r와 거듭 제곱 횟수를 나타내는 정수 n을 입력받아서 r^n을 구하여 화면에 출력한다.
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	double d; // 실수값
+	int n; // 거듭 제곱 횟수
+	double result = 1; // 결과값
+
+	printf("실수의 값을 입력하시오: ");
+	scanf_s("%lf", &d);
+
+	printf("거듭제곱 횟수를 입력하시오: ");
+	scanf_s("%d", &n);
+
+	for (int i = 0; i < n; i++) // 거듭제곱 횟수만큼 반복
+	{
+		result *= d; // 횟수만큼 실수값 곱해줌
+	}
+
+	printf("결과값은 %lf\n", result);
+
+	return 0;
+}
---
+++ C/C_Express/CH07_Programming/Ex_0711.c
@@ -0,0 +1,26 @@
+﻿/*
+0711.
+
+1^2 + 2^2 + 3^2 + ... + n^2의 값을 계산하여 출력하여 보자.
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int n; // 입력받을 정수
+	int result = 0; // 계산값
+
+	printf("n의 값을 입력하시오: ");
+	scanf_s("%d", &n);
+
+	for (int i = 1; i <= n; i++) // 1~n 반복
+	{
+		result += i * i; // 제곱값 구해서 더해줌
+	}
+
+	printf("계산값은 %d입니다.\n", result);
+
+	return 0;
+}
