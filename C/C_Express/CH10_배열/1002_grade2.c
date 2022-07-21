20649d744842441746f66970f7473aa3d8ddc02c
naylle98 <khb9827y@naver.com>
2022-07-21 20:58:53 +0900

    .

---
+++ C/C_Express/CH10_배열/1002_grade2.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <stdlib.h>	
+#define SIZE 5 // 배열 크기를 나타내는 기호상수 SIZE 정의
+
+int main(void)
+{
+	int i;
+	int grade[SIZE]; // 배열 선언
+
+	for (i = 0; i < SIZE; i++)
+		grade[i] = rand() % 100; // 배열 원소에 난수를 채운다.
+
+	for (i = 0; i < SIZE; i++)
+		printf("grade[%d] = %d\n", i, grade[i]);
+
+	return 0;
+}
---
+++ C/C_Express/CH10_배열/1004_grade4.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+#define STUDENTS 10 // 기호 상수로 학생들의 수를 나타냄. 후에 학생들의 수 변경 시 값만 변경하면 됨
+
+int main(void)
+{
+	int grade[STUDENTS]; // 각 학생들의 성적을 저장할 크기가 10인 정수형 배열을 선언
+	int sum = 0; // 성적의 합계를 저장할 변수 sum을 선언. sum에는 성적들이 계속 더해지므로 초기값은 0이어야 함
+	int i, average;
+
+	for (i = 0; i < STUDENTS; i++) 
+	{
+		printf("학생들의 성적을 입력하시오: ");
+		scanf_s("%d", &grade[i]);
+	}
+
+	for (i = 0; i < STUDENTS; i++)
+		sum += grade[i];
+
+	average = sum / STUDENTS;
+	printf("학생 평균 = %d\n", average);
+
+	return 0;
+}
