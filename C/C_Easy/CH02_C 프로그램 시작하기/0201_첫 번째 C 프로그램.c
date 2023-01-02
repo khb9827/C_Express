f3427b97682241bdeb7dd48d111403f3fedd0c85
naylle98 <khb9827y@naver.com>
2023-01-02 23:26:13 +0900

    .

---
+++ C/C_Easy/CH02_C 프로그램 시작하기/0201_첫 번째 C 프로그램.c
@@ -0,0 +1,9 @@
+﻿/* Ex 02_01.c */ // 주석
+#include <stdio.h> // 입출력 라이브러리 사용을 위한 준비
+
+int main(void) // 프로그램 시작점의 함수
+{
+	printf("Hello World\n"); // 출력
+
+	return 0; 
+}
---
+++ C/C_Easy/CH02_C 프로그램 시작하기/0202_두 번째 C 프로그램.c
@@ -0,0 +1,19 @@
+﻿/* Ex 02_02.c */
+#include <stdio.h>
+
+int main(void)
+{
+	float num; // 실수형 변수 선언
+	int i_part; // 정수형 변수 선언
+	float f_part; // 실수형 변수 선언
+
+	printf("실수를 입력하세요: ");
+	scanf_s("%f", &num); // 실수형 변수 입력		
+
+	i_part = num;
+	f_part = num - i_part;
+
+	printf("%f의 정수부는 %d이고, 실수부는 %f입니다.\n", num, i_part, f_part);
+
+	return 0;
+}
