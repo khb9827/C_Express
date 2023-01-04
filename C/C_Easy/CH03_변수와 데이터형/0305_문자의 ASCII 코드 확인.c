583c1c1dba8ad6b516ed67566a2f00f2a87b5e6a
naylle98 <khb9827y@naver.com>
2023-01-04 21:10:03 +0900

    .

---
+++ C/C_Easy/CH03_변수와 데이터형/0305_문자의 ASCII 코드 확인.c
@@ -0,0 +1,12 @@
+﻿/* Ex 03_05.c */
+#include <stdio.h>
+
+int main(void)
+{
+	char ch;  // char형 변수 선언
+
+	scanf_s("%c", &ch);
+	printf("%c 문자의 ASCII 코드 : %d (%x) \n", ch, ch, ch);
+
+	return 0;
+}
---
+++ C/C_Easy/CH03_변수와 데이터형/0306_signed와 unsigned의 비교.c
@@ -0,0 +1,13 @@
+﻿/* Ex 03_06.c */
+#include <stdio.h>
+
+int main(void)
+{
+	short num1 = -10; // 부호 있는 정수, 최상위 비트 음수이므로 1로 설정
+	unsigned short num2 = num1; // 부호 없는 정수, 최상위 비트 양수이므로 값 저장
+
+	printf("부호 있는 정수 : %d\n", num1);
+	printf("부호 없는 정수 : %d\n", num2);
+
+	return 0;
+}
